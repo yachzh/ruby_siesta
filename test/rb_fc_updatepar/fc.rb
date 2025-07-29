4e9ac25686cf09d5012b69cb1c1d5b20ac06b0a6
yachao <yachao.zhang@pku.edu.cn>
2025-07-29 13:33:32 +0800

    updated at 2025-07-29 13:33:32

---
+++ test/rb_fc_updatepar/fc.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+$LOAD_PATH << '../../lib'
+require 'siesta'
+require 'phys'
+require 'time_converter'
+
+magcenter = 'fe'
+U = 4.0
+xc = 'LDA'
+resultfile = File.open('result.txt', 'w')
+energies = {}
+
+%w[hs ls].each do |state|
+  start_time = Time.now
+  dft = Siesta.import_from_file("#{state}.STRUCT_OUT")
+  dft.xc(xc)
+  dft.label("#{xc}-U#{format('%.1f', U)}-#{state}")
+  dft.parameters(beta: 0.03, nmaxold: 5, dmtol: 1e-4,
+                 energyshift: '150 meV', temperature: '300 K',
+                 htol: '10 meV',
+                 meshcutoff: '500 Ry', filtercutoff: '300 Ry')
+  dft.spin(pol: true, fixspin: true)
+  dft.initial_spin(atom: magcenter, spin_state: state)
+  dft.plus_u(atom: magcenter, orbital: '3d', hubbard_u: U)
+  energy = dft.energy
+  energies[state] = energy
+  end_time = Time.now
+  walltime = TimeConverter.sec2time(end_time - start_time)
+  resultfile.puts "#{state} #{format('%14.6f', energy)} Walltime: #{format('%10s', walltime)}"
+end
+
+e_hl = (energies['hs'] - energies['ls']) / (Phys.kJ / Phys.mol)
+resultfile.puts "E_hl = #{format('%5.1f', e_hl)} kJ/mol"
+resultfile.close
